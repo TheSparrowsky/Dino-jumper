29b4f2379963d599feda9f49580ca5c15cfd7148
Kamil Wróblewski <k.wroblewski54@gmail.com>
2021-03-13 21:45:23 +0100

    Add cactuses spawner and shredder

---
+++ Assets/Scripts/Shredder.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Shredder : MonoBehaviour{
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Destroy(other.gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+
+    [SerializeField]private GameObject objectToSpawn;
+    [SerializeField]private int howManyObstacles = 3;
+
+    private int obstaclesCount;
+    private float timer = 0.0f;
+
+    private float[] timeToCreate = new float[] {0.15f, 2f};
+
+    private int randomTime = 0;
+
+    private void Start() {
+        obstaclesCount = GameObject.FindGameObjectsWithTag(objectToSpawn.tag).Length;
+    }
+
+    void Update(){
+        timer += Time.deltaTime;
+
+        Debug.Log(randomTime);
+
+        obstaclesCount = GameObject.FindGameObjectsWithTag(objectToSpawn.tag).Length;
+        if(obstaclesCount < howManyObstacles && timer >= timeToCreate[randomTime]){
+            if(randomTime == 0 && obstaclesCount > 2) randomTime = 1;
+            randomTime = Random.Range(0, 2);
+            Instantiate(objectToSpawn, transform.position, transform.rotation);
+            timer = 0f;
+        }
+
+    }
+}
