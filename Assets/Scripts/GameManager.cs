1c7596e7cd19a10ca5aea2932eae595b696ababc
Kamil Wróblewski <k.wroblewski54@gmail.com>
2021-03-17 10:00:28 +0100

    Modified UI elements, add exit button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ public class GameManager : MonoBehaviour{
 
     [SerializeField]private UIManager uIManager;
 
+    public void gameExit(){
+        Application.Quit();
+    }
+
     public void gameRestart(){
         SceneManager.LoadScene(0);
         Time.timeScale = 1;
